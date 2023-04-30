8b9b7b69dc73ef9f761af2f079b1be29c0b96c4a
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-04-30 19:04:15 +0500

    vok

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var result = document.getElementById("result");
+
+function appendToResult(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+}
+
+function calculateResult() {
+  result.value = eval(result.value);
+}
+
